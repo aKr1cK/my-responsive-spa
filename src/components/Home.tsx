7da6fd79784fae47821fa52bef582c6fb6629621
aKr1cK <akrick89@gmail.com>
2024-10-26 02:04:06 +0530

    added data

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,24 +4,11 @@ import React, { useState } from 'react';
 import { Button, Card, Container, Nav } from 'react-bootstrap';
 import { FaGreaterThan } from 'react-icons/fa';
 import { IoMdAdd } from 'react-icons/io';
+import Truck from './Truck';
 
 const Home = () => {
   const [activeTab, setActiveTab] = useState('Truck');
-  const [rowData, setRowData] = useState([
-    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-    { make: "Ford", model: "F-Series", price: 33850, electric: false },
-    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-  ]);
-  const [colDefs, setColDefs]: any[] = useState([
-    { field: "make" },
-    { field: "model" },
-    { field: "price" },
-    { field: "electric" },
-    { field: "make" },
-    { field: "model" },
-    { field: "price" },
-    { field: "electric" }
-  ]);
+  
   return (
     <Container className="text-center">
       <div className="text-start">Home&nbsp;&nbsp;<FaGreaterThan />&nbsp;&nbsp;{activeTab}&nbsp;&nbsp;{/*<FaGreaterThan />&nbsp;&nbsp;New Shipment*/}</div>
@@ -37,15 +24,8 @@ const Home = () => {
           </Nav>
         </Card.Header>
         <Card.Body>
-          <div className="text-start"><IoMdAdd size={30}  style={{ background: "#0d6efd" }}/></div>
-          {activeTab == 'Truck' &&
-          <div className="ag-theme-alpine-dark pt-2" style={{ height: 900 }}>
-            <AgGridReact rowData={rowData} columnDefs={colDefs}/>
-          </div>
-          }
-          {
-            activeTab == 'Other' &&  <div>OTHER</div>
-          }
+          {activeTab == 'Truck' && <Truck/>}
+          {activeTab == 'Other' &&  <div>OTHER</div>}
         </Card.Body>
       </Card>
     </Container>
